c64a30b4479c65cb5d2433f0eb425250efed4402
TheNT <nt@eigh.tk>
2017-12-14 10:40:53 +0000

    merging into master

--- filter/thread/Filter.js
+++ filter/thread/Filter.js
@@ -1,3 +1,4 @@
+const { formatProp } = require('../lib');
 /** A thread contains global information about the content of the whole page and its content. A thread can contain multiple posts grouped together.*/
 module.exports = class {
   constructor({
@@ -8,32 +9,32 @@ module.exports = class {
     this.crawled = crawled;
   }
   * props(){
-    yield* ['thread.title','thread.section_title','thread.url','thread.published','spam_score','crawled']
+    yield* ['thread.title','thread.url','thread.published','spam_score','crawled']
   }
+  
+  get ['thread.title'](){ return this.thread.title}
+  get ['thread.url'](){ return this.thread.url }
+  get ['thread.published'](){ return this.thread.published}
   
   toString(){
     let result = '';
+    if(this.thread.section_title !== undefined){
+      result += ' AND thread.section_title:'.concat(this.thread.section_title);
+    }
+    for( let prop of this.props()){
+      if(this[prop] === undefined)continue;
+      switch(prop){
+        case 'thread.url':
+          result += formatProp(prop, this[prop].replace(/(:|\/)/g,'\\$1')); break;
+        case 'thread.title':
+          this[prop].forEach( (val) => {
+            result += val.startsWith('-') ? formatProp('-'.concat(prop),val.substr(1)) : formatProp(prop,val);
+          });break;
+        default:
+          result += formatProp(prop, this[prop]);
+      }
+    }
     return result;
   }
   
 }
-//TODO negative titles
-
-/*
- * thread.title
-
-Search for posts containing the word "glass" and not "metal" in their title:
-thread.title:glass -thread.title:metal
-thread.section_title
-
-Search for the posts containing the word food only under sections with a title that contains the word "restaurants":
-food AND thread.section_title:restaurants
-thread.url
-
-spam_score
-
-thread.published
-
-crawled
-
- */
--- test/filter.js
+++ test/filter.js
@@ -1,11 +1,7 @@
-const PostFilter = require('../filter/post/Filter');
-const ThreadFilter = require('../filter/thread/Filter');
-const SocialFilter = require('../filter/social/Builder');
-const SiteFilter = require('../filter/site/Builder')
+const Filter = require('../filter')
 
-let filter1 = new PostFilter({language:['english'], author:'NT'})
-let filter2 = new PostFilter({language:['germant','italian'], has_video:true});
-console.log(new ThreadFilter({}))
-console.log(
-  new SocialFilter({}).pinterest(10).build().toString()
-)
+
+let filter = new Filter('jj');
+filter.thread().section_title('rest')
+filter.post().author('somebody').has_video(false)
+console.log(filter.query)
