b14f85fffb225c3283d98aa92a2489e773ccc1fb
TheNT <nt@eigh.tk>
2017-12-19 12:55:57 +0000

    still don't know how to combine git-tags with npm version

--- test/filter.js
+++ test/filter.js
@@ -1,7 +1,8 @@
-const Filter = require('../lib/filter')
-
+const { Filter } = require('../lib');
+const { Sentiment } = require('../lib/filter/entitiesSentiment/Filter')
 
 let filter = new Filter('jj');
 filter.thread().section_title('rest')
 filter.post().author('somebody').has_video(false)
+filter.entitySentiment().person(new Sentiment({type:'positiv',value:'obama'}))
 console.log(filter.query)
